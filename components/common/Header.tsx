ce2790795c407be381480ca9b3a872ec9ad425c9
realmind87 <realmind87@naver.com>
2023-01-25 09:46:40 +0900

    commit

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,24 @@
 import { AiFillAliwangwang } from 'react-icons/ai'
 import { IoIosArrowDropleftCircle } from "react-icons/io";
+import Logo from "@/public/images/common/logo.png"
+import Image from 'next/image'
+
 
 const Header = () => {
     return (
-        <header>
-            header
+        <header className='header'>
+            <h1 className='logo'>
+                <Image 
+                    src={Logo} 
+                    alt="Logo"
+                />
+            </h1>
+            <div className="switch-wrapper">
+                <input type="checkbox" id="switch-point" />
+                <label htmlFor="switch-point" className="switch-label">
+                    <span className="onf-btn"></span>
+                </label>
+            </div>
         </header>
     )
 }
--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,6 @@
 //declare module 'react-drawer';
+
+declare module '*.png' {
+    const content: import('../public/images').StaticImageData
+    export default content
+}
